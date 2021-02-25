bbe0b8e3a986ec7bcffbe0d1afd861e2acac5fad
Niklas Karl <Niklas.Karl@p3-group.com>
2021-02-25 13:11:32 +0100

    Improve test document generation.

--- src/BinaryMesh.OpenXml.Explorer/Program.cs
+++ src/BinaryMesh.OpenXml.Explorer/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 using BinaryMesh.OpenXml.Presentations;
+using BinaryMesh.OpenXml.Spreadsheets;
 
 namespace BinaryMesh.OpenXml.Explorer
 {
@@ -9,6 +12,36 @@ namespace BinaryMesh.OpenXml.Explorer
     {
         static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                switch (args[0])
+                {
+                    case "presentation":
+                        GeneratePresentation(args.Skip(1).ToArray());
+                        break;
+                    case "spreadsheet":
+                        GenerateSpreadsheet(args.Skip(1).ToArray());
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("mode not specified");
+            }
+        }
+
+        private static void GeneratePresentation(string[] args)
+        {
+            string destination;
+            if (args.Length > 0)
+            {
+                destination = args[0];
+            }
+            else
+            {
+                Console.WriteLine("destination not specified");
+                return;
+            }
 
             IPresentation presentation = null;
             using (Stream source = typeof(Program).Assembly.GetManifestResourceStream("BinaryMesh.OpenXml.Explorer.Assets.ExamplePresentation.pptx"))
@@ -21,21 +54,73 @@ namespace BinaryMesh.OpenXml.Explorer
                 ISlide slide = presentation.InsertSlide(presentation.SlideMasters[0].SlideLayouts[0]);
 
                 slide.VisualTree["Titel 1"].AsShapeVisual()
-                    .SetText("Statusbericht Versionskontrolle");
+                    .SetText("Automated Presentation Documents made easy");
 
                 slide.VisualTree["Untertitel 2"].AsShapeVisual()
-                    .SetText("VW310 ID.3");
+                    .SetText("BinaryMesh.OpenXml is an open-source library to easily and intuitively create OpenXml documents");
 
                 slide.VisualTree["Datumsplatzhalter 3"].AsShapeVisual()
-                    .SetText("22.02.2021");
+                    .SetText("10.10.2020");
 
-                using (Stream destination = new FileStream("C:\\Users\\krq\\Desktop\\TestPresentation.pptx", FileMode.Create, FileAccess.ReadWrite))
+                using (Stream stream = new FileStream(destination, FileMode.Create, FileAccess.ReadWrite))
                 {
-                    presentation.Close(destination);
+                    presentation.Close(stream);
                 }
             }
 
-            Console.WriteLine("Hello World!");
+            new Process
+            {
+                StartInfo = new ProcessStartInfo(destination)
+                {
+                    UseShellExecute = true
+                }
+            }.Start();
+        }
+
+        private static void GenerateSpreadsheet(string[] args)
+        {
+            string destination;
+            if (args.Length > 0)
+            {
+                destination = args[0];
+            }
+            else
+            {
+                Console.WriteLine("destination not specified");
+                return;
+            }
+
+            using (ISpreadsheetDocument document = SpreadsheetFactory.CreateSpreadsheet())
+            {
+                IWorksheet sheet = document.Workbook.AppendWorksheet("Sheet 1");
+                sheet.Cells[0, 0].SetValue("1. Quarter");
+                sheet.Cells[1, 0].SetValue("2. Quarter");
+                sheet.Cells[2, 0].SetValue("3. Quarter");
+                sheet.Cells[3, 0].SetValue("4. Quarter");
+
+                sheet.Cells[0, 1].SetValue(152306);
+                sheet.Cells[1, 1].SetValue(128742);
+                sheet.Cells[2, 1].SetValue(218737);
+                sheet.Cells[3, 1].SetValue(187025);
+
+                sheet.Cells[0, 2].SetValue(90123);
+                sheet.Cells[1, 2].SetValue(120744);
+                sheet.Cells[2, 2].SetValue(218681);
+                sheet.Cells[3, 2].SetValue(187322);
+
+                using (Stream stream = new FileStream(destination, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    document.Close(stream);
+                }
+            }
+
+            new Process
+            {
+                StartInfo = new ProcessStartInfo(destination)
+                {
+                    UseShellExecute = true
+                }
+            }.Start();
         }
     }
 }
